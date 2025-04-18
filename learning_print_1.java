31d5298549e1eb9ee5b2bfdd7ed1affbf3521843
Prakash Kumar Thuthika <prakash.thuthika@gmail.com>
2025-04-18 23:00:28 +0530

    Create learning_print_1.java

---
+++ learning_print_1.java
@@ -0,0 +1,9 @@
+
+public class Main{
+    public static void main(String[] args){
+        System.out.print(6);
+        System.out.print("\n");
+        System.out.println(12/6);
+        System.out.print("Above is priniting ");
+    }
+}
