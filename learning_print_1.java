943a84589ee727c13d5580904c82370585fe2ca8
Prakash Kumar Thuthika <prakash.thuthika@gmail.com>
2025-04-18 23:17:39 +0530

    Update learning_print_1.java

--- learning_print_1.java
+++ learning_print_1.java
@@ -1,9 +1,36 @@
-
-public class Main{
+class Main{
     public static void main(String[] args){
+        System.out.print(12);
+        System.out.print("\n");
         System.out.print(6);
         System.out.print("\n");
         System.out.println(12/6);
-        System.out.print("Above is priniting ");
+        System.out.println("Done....");
+        int number1 = 100;  //byte short long 
+        System.out.println(number1);
+        System.out.println(number1 * 3);
+        long number2 = 120;
+        float val = number2/number1;
+        System.out.println(val);
+        String a = "How are you"; 
+        System.out.println(a);
+        char c = 'f';
+        System.out.println(c);
+        boolean t = true;
+        System.out.println(t);
     }
+
+
+// 12
+// 6
+// 2
+// Done....
+// 100
+// 300
+// 1.0
+// How are you
+// f
+// true
+
+// === Code Execution Successful ===
 }
